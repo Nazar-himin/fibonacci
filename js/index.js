208fcd78cd50d29575c0161f1f7866724102bdf9
Nazar <nazar.himin@gmail.com>
2018-10-03 13:22:14 +0000

    simplehard

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
 const num = parseFloat(prompt('What number of Fibonacci do you search?'));
-alert("for method " + fiboFor(num));
-
+alert("for method. Your number - " + fiboFor(num));
+alert("while method. Your number - " + fiboWhile(num));
 
 
 function fiboFor(num) {
-    let fibOne = 1, fibTwo = 1;
-    for(let i = 3; i <= num; i++){
-    let fibNextNum = fibOne + fibTwo;
-    fibOne = fibTwo;
-    fibTwo = fibNextNum;
+    let fibOne = 1,
+        fibTwo = 1;
+    for (let i = 3; i <= num; i++) {
+        let fibNextNum = fibOne + fibTwo;
+        fibOne = fibTwo;
+        fibTwo = fibNextNum;
+    }
+    return fibTwo;
 }
-return fibTwo;
+
+function fiboWhile(num) {
+    let fibOne = 1, fibTwo = 1, counter = 3;
+    while (counter <= num) {
+        let fibNextNum = fibOne + fibTwo;
+        fibOne = fibTwo;
+        fibTwo = fibNextNum;
+        counter++;
+        
+    }
+    return fibTwo;
 }
+    
