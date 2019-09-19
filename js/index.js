5362301cde81783e880c2fc0c198fc0820628355
Nazar-himin <43187901+Nazar-himin@users.noreply.github.com>
2019-09-19 23:17:19 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,49 +1,43 @@
-const num = parseFloat(prompt('What number of Fibonacci do you search?'));
-alert("for method. Your number - " + fiboFor(num));
-alert("while method. Your number - " + fiboWhile(num));
-alert("reduce method. Your number - " + fiboReduce(num));
-alert("array method. Your number - " + fiboArray(num));
-
-
+const num = parseInt(prompt('What number of Fibonacci do you search?'));
+alert('Loop method. Your number - ' + fiboFor(num));
+alert('While method. Your number - ' + fiboWhile(num));
+alert('Array method. Your number - ' + fiboArray(num));
+alert('Recursive method. Your number - ' + fiboRec(num));
 function fiboFor(num) {
-    let fibOne = 1,
-        fibTwo = 1;
-    for (let i = 3; i <= num; i++) {
-        let fibNextNum = fibOne + fibTwo;
-        fibOne = fibTwo;
-        fibTwo = fibNextNum;
-    }
-    return fibTwo;
+  let fibOne = 1,
+    fibTwo = 1;
+  for (let i = 3; i <= num; i++) {
+    let fibNextNum = fibOne + fibTwo;
+    fibOne = fibTwo;
+    fibTwo = fibNextNum;
+  }
+  return fibTwo;
 }
 
 function fiboWhile(num) {
-    let fibOne = 1, fibTwo = 1, counter = 3;
-    while (counter <= num) {
-        let fibNextNum = fibOne + fibTwo;
-        fibOne = fibTwo;
-        fibTwo = fibNextNum;
-        counter++;
-        
-    }
-    return fibTwo;
-}
-
-function fiboReduce(num) {
-    let fiboRed = [1, 1];
-    for (let i = 3; i <= num; i++) {
-       let sum = fiboRed[0] + fiboRed[1];
-       fiboRed[0] = fiboRed[1];
-       fiboRed[1] = sum;
-    }
-    return fiboRed.reduce( (acc, el) => el );    
+  let fibOne = 1,
+    fibTwo = 1,
+    counter = 3;
+  while (counter <= num) {
+    let fibNextNum = fibOne + fibTwo;
+    fibOne = fibTwo;
+    fibTwo = fibNextNum;
+    counter++;
+  }
+  return fibTwo;
 }
 
 function fiboArray(num) {
-    let fibArr = [1, 1];
-    for (let i = 3; i <= num; i++) {
-        let sum = fibArr[0] + fibArr[1];
-        fibArr[0] = fibArr[1];
-        fibArr[1] = sum;
-    }
-    return fibArr[1];
+  let fibArr = [1, 1];
+  for (let i = 2; i < num; i++) {
+    fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+  }
+  return fibArr.pop();
+}
+
+function fiboRec(num) {
+  if (num <= 2) {
+    return 1;
+  }
+  return fiboRec(num - 1) + fiboRec(num - 2);
 }
